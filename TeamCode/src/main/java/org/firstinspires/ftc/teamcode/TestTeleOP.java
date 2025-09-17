144fd6017d09a93d2d1b7a2c58e4f0ae28ecca5a
Ciro <165612945+Cirooo823@users.noreply.github.com>
2025-09-16 19:23:41 -0600

    Created repo and added basic mecanum functions.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOP.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name="Test TeleOP", group="TeleOp")
+public class TestTeleOP extends OpMode {
+
+    public DcMotorEx right_b;
+    public DcMotorEx left_f;
+    public DcMotorEx right_f;
+    public DcMotorEx left_b;
+
+    public double drivePowerScale;
+
+    @Override
+    public void init() {
+        left_f = hardwareMap.get(DcMotorEx.class, "leftFront");
+        right_f = hardwareMap.get(DcMotorEx.class, "rightFront");
+        left_b = hardwareMap.get(DcMotorEx.class, "leftBack");
+        right_b = hardwareMap.get(DcMotorEx.class, "rightBack");
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+        double x = -gamepad1.right_stick_x;
+        double y = -gamepad1.left_stick_y;
+        double rx = -gamepad1.left_stick_x;
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        right_f.setPower(((y + x + rx) / denominator) * drivePowerScale);
+        left_b.setPower(((y - x + rx) / denominator) * drivePowerScale);
+        left_f.setPower(((y - x - rx) / denominator) * drivePowerScale);
+        right_b.setPower(((y + x - rx) / denominator) * drivePowerScale);
+    }
+}
