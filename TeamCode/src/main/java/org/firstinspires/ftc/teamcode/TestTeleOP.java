8c7aa7e6a9a9951cba588cb0b3e75b384ce48e8b
Ciro <165612945+Cirooo823@users.noreply.github.com>
2025-09-17 15:40:43 -0600

    renamed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOP.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name="LoliGoat", group="TeleOp")
+@TeleOp(name="Test TeleOP", group="TeleOp")
 public class TestTeleOP extends OpMode {
 
     public DcMotorEx right_b;
