a4d878e92dc6755c36f5cf4c2edcbb4167c41945
26dolores <26dolparenti@asfg.edu.mx>
2025-10-04 11:55:00 -0600

    Testing push/pull

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -8,9 +8,13 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
@@ -30,8 +34,23 @@ public class Constants {
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(5.275) //X distance from center of the one that rolls forwards/back, in inches
+            .strafePodX(-1.5)//Y distance from center of the one that rolls left/right, in inches
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")// change name to what we named the pinpoint in the controller
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+    //
+    // TO REVERSE ENCODER:
+    // .forwardEncoderDirection(Encoder.REVERSE)
+    /// / and/or:
+    //.strafeEncoderDirection(Encoder.REVERSE)
+
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
                 .build();
